740a66188d3dd721195b3d0a61b7390bcd350320
manjusha <manjusha@hamon.in>
2022-04-12 16:00:16 +0530

    changing API's to a separate folder

--- lyrics_web.py
+++ lyrics_web.py
@@ -1,34 +1,14 @@
 from distutils.log import debug
 from flask import Flask, render_template, jsonify
-import getdata
 app = Flask(__name__)
+
+import api
+
+app.register_blueprint(api.api, url_prefix="/api")
 
 @app.route("/")
 def hello():
     return render_template("base.html",)
-
-@app.route("/artists")
-def get_artists():
-    artists = getdata.get_all_artist()
-    artists_arr = [{'id':i[0], "name":i[1]} for i in artists]
-    return jsonify(artists_arr)
-
-@app.route("/songs/<int:aid>")
-def list_all_songs(aid):
-    songs= getdata.get_all_songs(aid)
-    songs_arr = [{'id':i[1], "name":i[0]} for i in songs]
-    # singer= getdata.singer(aid)
-    # artists = getdata.get_all_artist()
-    return jsonify(songs_arr)
-
-@app.route("/songs/<int:aid>/lyrics/<int:sid>")
-def lyrics(sid,aid):
-    lyrics= getdata.get_lyrics(sid)
-    songs= getdata.get_all_songs(aid)
-    singer= getdata.singer(aid)
-    artists = getdata.get_all_artist()
-    print(lyrics)
-    return jsonify(lyrics)
 
 if __name__=="__main__":
     app.run(debug=True) 
